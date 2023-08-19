07bbfb94e6d10a9d66b5f82aeabf8181cfcb73fc
Killerbeez7 <plamen.tsvetkov96@gmail.com>
2023-08-19 17:13:49 +0300

    create models for recipe, likes

--- recipe_site_django/recipe_app/models.py
+++ recipe_site_django/recipe_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    NAME_MAX = 20
+    DESC_MAX = 100
+
+    name = models.CharField(
+        max_length=NAME_MAX,
+        null=True,
+        blank=True,
+    )
+
+    description = models.TextField(
+        max_length=DESC_MAX,
+        null=True,
+        blank=True,
+    )
+
+    preparation_time = models.PositiveIntegerField(
+        models.Min(1),
+        null=True,
+        blank=True,
+    )
+
+    image = models.ImageField(
+        upload_to='recipe_images',
+    )
+
+
+class Like(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
