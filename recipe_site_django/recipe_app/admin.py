9f8345c2774220809cf2c418d8e88d8b5e1b6233
Killerbeez7 <plamen.tsvetkov96@gmail.com>
2023-08-21 13:48:41 +0300

    add list display and filter in admin panel

--- recipe_site_django/recipe_app/admin.py
+++ recipe_site_django/recipe_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipe_site_django.recipe_app.models import Recipe
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'preparation_time', 'likes_count')
+    list_filter = ('name',)
+
+    def likes_count(self, obj):
+        return obj.like_set.count()
+
+
+admin.site.register(Recipe, RecipeAdmin)
